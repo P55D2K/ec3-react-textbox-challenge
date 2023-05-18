61ca95ff56cb7325e1ae00955bd737a7232aafdf
ZK <231637w@student.hci.edu.sg>
2023-05-18 16:37:29 +0800

    sd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-// import React from 'react';
-// import { useState } from 'react';
-
-// function App() {
-//   const [text, setText] = useState('');
-
-//   return (
-//     <div className="bg-black h-screen w-screen pt-[50px]">
-//       <div className='text-white text-4xl text-center h-[500px] flex items-center justify-center mx-auto w-[400px] rounded-md' style={{backgroundColor:'#0B3D2E'}}>
-//         <div className='text-4xl text-white text-center w-full'> Enter your text here </div>
-//         <span className='text-2xl text-white text-center w-full'>Your text but reversed: <span id='reversedtext'></span></span> <br />
-//         <input type='text' className='bg-transparent border-none outline-none text-4xl text-white text-center w-full' onChange={() => {
-//           setText(document.getElementById('text').value);
-//           document.getElementById('reversedtext').innerHTML = text.split("").reverse().join("");
-//         }} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState } from 'react';
 
-function App() {
-  const [reversedText, setReversedText] = useState('');
+const App = () => {
+  const [ reversedText, setReversedText ] = useState('');
 
   return (
     <div className="bg-black h-screen w-screen pt-[50px]">
